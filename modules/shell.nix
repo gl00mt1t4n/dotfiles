3a0fa695c82e83b67f04cd5c5ee46a716098c396
gl00mt1t4n <gloomtitan1337@gmail.com>
2026-05-14 20:28:48 -0500

    shell.nix fix

--- modules/shell.nix
+++ modules/shell.nix
@@ -15,36 +15,7 @@
 
   ];
 
-  programs.bash = {
-    enable = true;
-    bashrcExtra = ''
-      eval "$(zoxide init bash)"
-      eval "$(fzf --bash)"
-      nixrebuild() {
-        case "$1" in
-          -system) cd /home/gl00m/dotfiles && make system ;;
-          -user)   cd /home/gl00m/dotfiles && make user ;;
-          *)       cd /home/gl00m/dotfiles && make full ;;
-        esac
-      }
-    '';
-    shellAliases = {
-      dotfiles = "cd /home/gl00m/dotfiles";
-      nixconf  = "sudo nvim /home/gl00m/dotfiles/configuration.nix";
-      hyprconf = "nvim /home/gl00m/dotfiles/config/hypr/hyprland.conf";
-      ls       = "eza --icons";
-      ll       = "eza -la --icons";
-      cat      = "bat";
-    };
-  };
-
-  programs.starship = {
-	enable = true;
-	enableBashIntegration = true;
-	settings = {
-		add_newline = true;
-		character = {
-			success_symbol = "[❯](green)";
+  "[❯](green)";
         error_symbol = "[❯](red)";
       };
       directory = {
@@ -54,5 +25,3 @@
     };
   };
 
-  }
-}
